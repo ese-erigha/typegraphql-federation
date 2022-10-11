99f65a2b7f7d61bf378c4f29ab52c785615737c0
Eseoghene Erigha <ese.erigha@gmail.com>
2022-10-11 22:04:00 +0100

    refactor: cleanup common package

--- apps/gateway/src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import Router from '@koa/router';
-
-export const router = new Router();
-
-router.get(['/', '/health', '/check'], ctx => {
-  ctx.status = 200;
-  ctx.body = 'OK';
-});
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,1 @@
-export * from './graphql';
-export * from './middleware';
 export * from './zod';
